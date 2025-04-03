d4c053b2af6a6993bcffaf84f9f2abdecba1363c
JONAS LUIZ MARIN <jonas_marin@estudante.sesisenai.org.br>
2025-04-03 09:10:33 -0300

    adicionando movimentação

---
+++ shoot_class.js
@@ -0,0 +1,44 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+    des_obj(){
+        let img = new Image()
+        img.src = this.a 
+        des.drawImage(img,this.x,this.y,this.w,this.h)
+    }
+
+    colid(objeto) {
+        if((this.x < objeto.x + objeto.w)&&
+            (this.x + this.w > objeto.x)&&
+            (this.y < objeto.y + objeto.h)&&
+            (this.y + this.h > objeto.y)){
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
+class Player extends Obj{
+    dirY = 0
+    dirX = 0
+    vida = 5
+    pontos = 0
+
+    move(){
+        this.x += this.dirX
+        this.y += this.dirY
+        if(this.x <= +6){
+            this.x = 6
+        }else if(this.x >= 510){
+            this.x = 510
+        }
+    }
+}
+
+
--- shoot_main.js
+++ shoot_main.js
@@ -1,6 +1,47 @@
-atualiza()
+let des = document.getElementById("des").getContext("2d")
 
-desenha()
+let player = new Player(150,250,100,50,"./assets/nave.png")
+
+document.addEventListener('keydown',(e)=>{
+    // console.log(e.key)
+    if(e.key === 'ArrowUp'){
+        player.dirY = -7
+    }else if(e.key === 'ArrowDown'){
+        player.dirY = 7
+    }
+})
+document.addEventListener('keyup', (e)=>{
+    if(e.key === 'ArrowUp'){
+        player.dirY = 0
+    }else if(e.key === 'ArrowDown'){
+        player.dirY = 0
+    }
+})
+
+document.addEventListener('keydown',(e)=>{
+    // console.log(e.key)
+    if(e.key === 'ArrowRight'){
+        player.dirX = 7
+    }else if(e.key === 'ArrowLeft'){
+        player.dirX = -7
+    }
+})
+document.addEventListener('keyup', (e)=>{
+    if(e.key === 'ArrowRight'){
+        player.dirX = 0
+    }else if(e.key === 'ArrowLeft'){
+        player.dirX = 0
+    }
+})
+
+function atualiza(){
+    player.move()
+
+}
+
+function desenha(){
+    player.des_obj()
+}
 
 function main(){
     des.clearRect(0,0,1050,700)
