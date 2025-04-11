e5a5b9358ec0c3ffed9f56808dbde19025b68897
Mateus Queiroz Logrado <mateus_logrado@estudante.sesisenai.org.br>
2025-04-11 07:55:27 -0300

    arrumando index

--- iniciar.js
+++ iniciar.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown", function(event) {
     if (event.code === "Space") {
-        window.location.href = "index.html"
+        window.location.href = "jogo.html"
     }
     if (event.key.toLowerCase() === "h") {
         window.location.href = "instrucoes.html"
--- voltar.js
+++ voltar.js
@@ -1,5 +1,5 @@
 document.addEventListener("keydown", function(event) {
     if (event.key.toLowerCase() === "h") {
-        window.location.href = "menu.html"
+        window.location.href = "index.html"
     }
 })
